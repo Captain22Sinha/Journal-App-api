f2d3600bab22bf84eb445867bd73ef2cf005b4c1
Captain22Sinha <samratsinha7318@gmail.com>
2026-04-12 11:20:21 +0530

    @Service

--- src/main/java/net/edigest/journalApp/service/WeatherService.java
+++ src/main/java/net/edigest/journalApp/service/WeatherService.java
@@ -12,17 +12,16 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class WeatherService {
 
-    @Value("${weather_api_key}")
-    private String apiKey;
+    private String apiKey = "34f8609213270b153d32666c544ee86c";
 
-    private static final String API = "https://api.weatherstack.com/current?access_key=API_KEY&query=CITY";
+    private final String API = "https://api.weatherstack.com/current?access_key=API_KEY&query=CITY";
 
     @Autowired
     private RestTemplate restTemplate;
 
     public WeatherResponse getWeather(String city) {
         String finalAPI = API.replace("CITY", city).replace("API_KEY", apiKey);
-        ResponseEntity<WeatherResponse> response = restTemplate.exchange(finalAPI, HttpMethod.POST, null, WeatherResponse.class);
+        ResponseEntity<WeatherResponse> response = restTemplate.exchange(finalAPI, HttpMethod.GET, null, WeatherResponse.class);
         WeatherResponse body = response.getBody();
         return body;
     }
